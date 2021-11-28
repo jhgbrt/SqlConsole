99b4fc2685a5591eeb68d9ff72f733ed54172559
Jeroen Haegebaert <jeroen@haegebaert.com>
2021-11-28 18:43:57 +0100

    fix build

--- SqlConsole.Host/Properties/AssemblyInfo.cs
+++ SqlConsole.Host/Properties/AssemblyInfo.cs
@@ -1,9 +1,5 @@
-﻿global using System;
-global using System.Threading.Tasks;
-global using System.IO;
-global using System.Collections;
+﻿global using System.Collections;
 global using System.Collections.Generic;
-global using System.Linq;
 global using System.Data;
 global using System.Data.Common;
 global using System.Text;
